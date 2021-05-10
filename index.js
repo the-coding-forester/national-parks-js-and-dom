a0d07550389df57e836287962165f6e6e401a3c3
Stephanie Moore <smoore_forestry@outlook.com>
2021-05-09 22:52:17 -0400

    removing duplicate files

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-//holding text
+const descriptions = document.querySelectorAll(".description-display");
+
+for (let desc of descriptions.values()) {
+  let content = desc.innerText;
+
+  if (content.length > 250) {
+    content = content.slice(0, 250);
+    content = content + '<a href="#">...</a>';
+  }
+
+  desc.innerHTML = content;
+}
+
+const ratings = document.querySelectorAll(".rating-display .value");
+
+for (let rating of ratings) {
+  let ratingValue = parseFloat(rating.innerText);
+
+  if (ratingValue > 4.7) {
+    rating.classList.add("high-rating");
+    rating.classList.remove("value");
+  }
+}
+
+const parks = document.querySelectorAll(".park-display");
+const numberParks = parks.length;
+const newElement = document.createElement("div");
+
+newElement.innerText = `${numberParks} exciting parks to visit`;
+newElement.classList.add("header-statement");
+
+const header = document.querySelector("header");
+header.appendChild(newElement);
